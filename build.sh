b516207f98ea44eae3a3507839cbd29b535a1341
LordOsslor <oskar.weymann@student.hu-berlin.de>
2024-07-16 04:26:32 +0200

    Fix typo

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-podman build -t pynguin-sx -f ./Dockerfile --build-arg "CACHEBUST=$(date +%s)"
-podman build -t pynguin-hmx -f ./Dockerfile --build-arg "CACHEBUST=$(date +%s)" --build-arg "INJECT=crossover.py:/usr/local/lib/python3.10/site-packages/pynguin/ga/operators/crossover.py"
+podman build -t pynguin-sx -f ./Dockerfile --build-arg "CACHE_BUST=$(date +%s)"
+podman build -t pynguin-hmx -f ./Dockerfile --build-arg "CACHE_BUST=$(date +%s)" --build-arg "INJECT=crossover.py:/usr/local/lib/python3.10/site-packages/pynguin/ga/operators/crossover.py"
